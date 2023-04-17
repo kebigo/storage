5b5a4db7e8283ea95b5aa17126b5baa784613b17
kebigo <kebigoviajes@gmail.com>
2023-04-17 14:42:20 +0200

    pagina transportes implementada

--- 02-Web/js/buscador.js
+++ 02-Web/js/buscador.js
@@ -5,7 +5,7 @@ export class Buscador extends HTMLElement {
       <section class="buscador container-lg mx-auto d-flex justify-content-center align-items-center py-4 py-lg-0">
       <div class="buscador-filters  d-lg-flex justify-content-center align-items-center ">
           <div class="filter destino col d-flex flex-column align-items-start pb-3 pb-lg-0">
-              <label class="label-buscador fs-5 fw-semibold" for="destinoList">Destino<i class="label-icono fa-solid fa-location-dot"></i></label>
+              <label class="label-buscador fs-5 fw-semibold" for="destinoList">Destino&nbsp;<i class="label-icono fa-solid fa-location-dot"></i></label>
               <input class="input-buscador border-0 text-capitalize" list="destinos" type="text" id="destinoList" placeholder="Los Angeles, CA">
               <datalist id="destinos">
                   <option value="platano"></option>
@@ -15,7 +15,7 @@ export class Buscador extends HTMLElement {
           </div>
             
           <div class="filter transporte col d-flex flex-column pb-3 pb-lg-0 mt-3 mt-lg-0 me-5 me-lg-0">
-              <label class="label-buscador fs-5 fw-semibold" for="transportes">Fecha <i class="label-icono fa-solid fa-route"></i></label>
+              <label class="label-buscador fs-5 fw-semibold" for="transportes">Fecha&nbsp;<i class="label-icono fa-solid fa-route"></i></label>
               <input class="input-buscador border-0 text-capitalize" list="transportesList" type="text" id="transportes" placeholder="03/05 - 12/05">
               <datalist id="transportesList">
                   <option value="Avion"></option>
@@ -25,7 +25,7 @@ export class Buscador extends HTMLElement {
           </div>
 
           <div class="filter pasajeros col d-flex flex-column pb-3 pb-lg-0 mt-3 mt-lg-0">
-              <label class="label-buscador fs-5 fw-semibold" for="pasajeros">Pasajeros<i class="label-icono fa-solid fa-user"></i></label>
+              <label class="label-buscador fs-5 fw-semibold" for="pasajeros">Pasajeros&nbsp;<i class="label-icono fa-solid fa-user"></i></label>
               <input class="input-buscador border-0 text-capitalize" list="pasajerosList" type="text" id="pasajeros" placeholder="Adultos, 2">
               <datalist id="pasajerosList">
                   <option value="Avion"></option>
